86fe9ca3e9bb97ed82aa9a07436f552c315791cd
Jonathan Meyer <jon@gisjedi.com>
2017-10-05 15:14:39 -0400

    Added dj-database-url

--- boom/settings.py
+++ boom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,6 +132,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # If overriding ensure trailing slash is present, e.g. 'boom/'
 APP_CONTEXT = os.getenv('APP_CONTEXT', '')
+
+# Change 'default' database configuration with $DATABASE_URL.
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
